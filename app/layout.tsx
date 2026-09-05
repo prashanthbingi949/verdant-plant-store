d0629d217275d9c794815a7fad986f86f4645c27
Sai Prashanth Bingi <138649334+prashanthbingi949@users.noreply.github.com>
2026-09-05 23:49:40 +0530

    Load compact Verdant Way styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./home-overrides.css";
+import "./story-overrides.css";
 import { CartProvider } from "@/components/cart-provider";
 
 const geistSans = Geist({
