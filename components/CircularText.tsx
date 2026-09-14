d71f4004ef627c395b23c646010c45652af52400
Sai Prashanth Bingi <138649334+prashanthbingi949@users.noreply.github.com>
2026-09-15 01:24:59 +0530

    Use framer-motion directly for circular text

--- components/CircularText.tsx
+++ components/CircularText.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect } from "react";
-import { motion, useAnimation, useMotionValue } from "motion/react";
+import { motion, useAnimation, useMotionValue } from "framer-motion";
 import "./CircularText.css";
 
 type CircularTextProps = {
